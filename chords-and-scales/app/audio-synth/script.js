4ae84b755c53d2ba173e18064deee0aa25ddb618
vanely <vanelys94@gmail.com>
2023-04-29 11:46:30 -0400

    waves working waveform selection needs fixing

---
+++ chords-and-scales/app/audio-synth/script.js
@@ -0,0 +1,59 @@
+const startBtn = document.querySelector('.start')
+const stopBtn = document.querySelector('.stop')
+const volumeCtrl = document.querySelector('.volume-ctrl')
+const waveforms = document.getElementsByName('waveform')
+let selectedWaveform
+
+
+///////////////////////////// Setup Oscillator Synth /////////////////////////////
+const AudioContext = window.AudioContext
+const context = new AudioContext
+
+console.log(['Audio Context Object: ', AudioContext])
+console.log(['Audio Context Instance: ', context])
+
+// connect our context instance gain(volume control)
+// think of the connect method in the same way that modules are being connected to in modular synthesis
+const masterVolume = context.createGain()
+masterVolume.connect(context.destination)
+// starting volume
+masterVolume.gain.value = .1
+
+let oscillator
+
+///////////////////////////// Setup Event Listeners /////////////////////////////
+function changeVolume(event) {
+  // 'this' is the element responsible for volume control
+  masterVolume.gain.value = this.value
+}
+
+function setWaveform(event) {
+  // waveform selected from DOM to be set in oscillator
+  waveforms.forEach((waveform) => waveform.checked ? selectedWaveform = waveform : null)
+}
+
+volumeCtrl.addEventListener('input', changeVolume)
+
+startBtn.addEventListener('click', function(event) {
+  oscillator = context.createOscillator()
+  // setValueAtTime allows to set the frequency in Hz and start delay
+  oscillator.frequency.setValueAtTime(120, 0)
+  oscillator.connect(masterVolume)
+
+  oscillator.start(0)
+  oscillator.type = selectedWaveform
+})
+
+stopBtn.addEventListener('click', function(event) {
+  oscillator.stop(0)
+  delete oscillator
+})
+
+// waveform selection not working
+waveforms.forEach((waveform) => {
+  console.log(['Current Waveform', waveform])
+  waveform.addEventListener('change', function(event) {
+    setWaveform(event)
+    oscillator.type = selectedWaveform
+  })
+})
